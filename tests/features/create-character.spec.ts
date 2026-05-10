9772939e81bf7a120f8f3436398f7b4ee9a53c3c
Anton Marques Fedak <antonfedak@gmail.com>
2026-05-11 00:03:42 +0100

    Status Codes update

--- tests/features/create-character.spec.ts
+++ tests/features/create-character.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { authState } from '../client/auth-state';
 import { createCharacter } from '../client/character-client';
 import { BARBARIAN_CHARACTER_DATA, ROGUE_CHARACTER_DATA } from '../data/create-character-data';
-import { expectStatusCodeOk } from '../snippets/status-code-validators';
+import { expectStatusCodeCreated } from '../snippets/status-code-validators';
 
 test.describe.configure({ mode: 'serial' });
 
@@ -15,7 +15,7 @@ test('Create Rogue Character', {tag: ['@create', '@character', '@rogue']}, async
         ROGUE_CHARACTER_DATA,
     );
 
-    await expectStatusCodeOk(characterResponse);
+    await expectStatusCodeCreated(characterResponse);
     
     const character = await characterResponse.json();
 
@@ -35,7 +35,7 @@ test('Create Barbarian Character', {tag: ['@create', '@character', '@barbarian']
         BARBARIAN_CHARACTER_DATA,
     );
 
-    await expectStatusCodeOk(characterResponse);
+    await expectStatusCodeCreated(characterResponse);
 
     const character = await characterResponse.json();
 
